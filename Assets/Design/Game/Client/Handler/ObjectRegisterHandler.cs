5b4c6fd4b503cc29228a78dfd2aa9bf23eeab925
yinhuayong <wenyounyhy@gmail.com>
2020-05-29 16:12:27 +0800

    优化代码

---
+++ Assets/Design/Game/Client/Handler/ObjectRegisterHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Zyq.Game.Base;
+using UnityEngine.Networking;
+
+namespace Zyq.Game.Client
+{
+    public class ObjectRegisterHandler
+    {
+        public static void Register()
+        {
+            ObjectEventRegister.Ins.Register(ObjectEventRegister.AddObject, (NetworkBehaviour behaviour) =>
+            {
+                Debug.Log("Client:1111111111111111111111111111");
+            });
+
+            ObjectEventRegister.Ins.Register(ObjectEventRegister.RemoveObject, (NetworkBehaviour behaviour) =>
+            {
+                Debug.Log("Client:222222222222222222222222222222");
+            });
+        }
+
+        public static void Unregister()
+        {
+            ObjectEventRegister.Ins.Unregister(ObjectEventRegister.AddObject);
+            ObjectEventRegister.Ins.Unregister(ObjectEventRegister.RemoveObject);
+        }
+    }
+}
---
+++ Assets/Design/Game/Server/Handler/ObjectRegisterHandler.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using Zyq.Game.Base;
+using UnityEngine.Networking;
+
+namespace Zyq.Game.Server
+{
+    public class ObjectRegisterHandler
+    {
+        public static void Register()
+        {
+            ObjectEventRegister.Ins.Register(ObjectEventRegister.AddObject, (NetworkBehaviour behaviour) =>
+            {
+                Debug.Log("Server:1111111111111111111111111111");
+            });
+
+            ObjectEventRegister.Ins.Register(ObjectEventRegister.RemoveObject, (NetworkBehaviour behaviour) =>
+            {
+                Debug.Log("Server:1111111111111111111111111111");
+            });
+        }
+
+        public static void Unregister()
+        {
+            ObjectEventRegister.Ins.Unregister(ObjectEventRegister.AddObject);
+            ObjectEventRegister.Ins.Unregister(ObjectEventRegister.RemoveObject);
+        }
+    }
+}
