544d045daf753d19d6ca6b4b0f3bd26ce9c62f60
yinhuayong <wenyounyhy@gmail.com>
2020-06-11 23:54:57 +0800

    新增Attribute

---
+++ Assets/Design/Game/Base/Attribute/BroadcastAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Zyq.Game.Base
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class BroadcastAttribute : Attribute
+    {
+        public short MsgId;
+
+        public BroadcastAttribute(short msgId)
+        {
+            MsgId = msgId;
+        }
+    }
+}
--- Assets/Design/Game/Server/Server.cs
+++ Assets/Design/Game/Server/Server.cs
@@ -11,9 +11,14 @@ namespace Zyq.Game.Server
 
         private Dictionary<int, Connection> m_Connections;
 
-        public void Init()
+        private Server()
         {
             m_Connections = new Dictionary<int, Connection>();
+        }
+
+        public void Init()
+        {
+            m_Connections.Clear();
         }
 
         public void Dispose()
@@ -22,9 +27,18 @@ namespace Zyq.Game.Server
             {
                 connection.Dispose();
             }
-
             m_Connections.Clear();
-            m_Connections = null;
+        }
+
+        public void Broadcast(Connection target, NetworkWriter writer)
+        {
+            foreach (Connection connection in m_Connections.Values)
+            {
+                if (target != connection)
+                {
+                    connection.Send(writer);
+                }
+            }
         }
 
         public void OnStartServer()
@@ -47,7 +61,7 @@ namespace Zyq.Game.Server
 
         private void AddConnection(NetworkConnection net)
         {
-            if(!m_Connections.ContainsKey(net.connectionId))
+            if (!m_Connections.ContainsKey(net.connectionId))
             {
                 Connection connection = new Connection(net);
                 RegisterProtocols(connection);
@@ -58,7 +72,7 @@ namespace Zyq.Game.Server
         private void RemoveConnection(NetworkConnection net)
         {
             Connection connection = null;
-            if(m_Connections.TryGetValue(net.connectionId, out connection))
+            if (m_Connections.TryGetValue(net.connectionId, out connection))
             {
                 connection.Dispose();
                 m_Connections.Remove(net.connectionId);
