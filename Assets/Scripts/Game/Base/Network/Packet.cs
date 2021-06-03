8a75f75ad4308168bba0db98c2c170f20c2b7f00
yinhuayong <wenyounyhy@gmail.com>
2021-06-03 14:42:12 +0800

    优化目录结构

--- Assets/Scripts/Game/Base/Network/Packet.cs
+++ Assets/Scripts/Game/Base/Network/Packet.cs
@@ -12,15 +12,5 @@
             Buffer = buffer;
             Channel = channel;
         }
-
-        public void Read(byte[] buffer, int offset, int length)
-        {
-            Buffer.Read(buffer, offset, length);
-        }
-
-        public void Write(byte[] buffer, int offset, int length)
-        {
-            Buffer.Write(buffer, offset, length);
-        }
     }
 }
--- Assets/Scripts/Game/Base/Network/PacketProcessing.cs
+++ Assets/Scripts/Game/Base/Network/PacketProcessing.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using UnityEngine;
 using System.Collections.Generic;
 
 namespace Nice.Game.Base
